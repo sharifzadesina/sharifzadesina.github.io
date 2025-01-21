47d461046adddc77660738338f290155272c7455
Sina Sharifzade <sharifzadesina@gmail.com>
2025-01-21 23:41:29 +0330

    Fix routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import FullstackView from '@/views/FullstackView.vue';
 import BackendView from '@/views/BackendView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
